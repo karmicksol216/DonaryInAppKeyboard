23e5297b84c9d57d74143dc01e532c49db4e15d2
karmickdroid2 <karmickdroid2@gmail.com>
2019-08-07 17:27:34 +0530

    System Keyboard Added 7-8-2019 - numeric functional

--- app/src/main/java/com/karmic/customviews/CustomKeyboardView.java
+++ app/src/main/java/com/karmic/customviews/CustomKeyboardView.java
@@ -1,5 +1,6 @@
 package com.karmic.customviews;
 
+import android.app.Activity;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -8,9 +9,11 @@ import android.graphics.drawable.Drawable;
 import android.inputmethodservice.Keyboard;
 import android.inputmethodservice.Keyboard.Key;
 import android.inputmethodservice.KeyboardView;
+import android.text.Editable;
 import android.util.AttributeSet;
 import android.util.Log;
-import android.view.inputmethod.InputMethodSubtype;
+import android.view.View;
+import android.widget.EditText;
 
 import com.karmic.simplekeyboard.R;
 
@@ -21,10 +24,12 @@ public class CustomKeyboardView extends KeyboardView {
     static final int KEYCODE_OPTIONS = -100;
     static final int KEYCODE_LANGUAGE_SWITCH = -101;
     Context context;
+    //private Activity mHostActivity;
 
     public CustomKeyboardView(Context context, AttributeSet attrs) {
         super(context, attrs);
         this.context = context;
+        //mHostActivity= (Activity) context;
     }
     public CustomKeyboardView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
@@ -41,29 +46,38 @@ public class CustomKeyboardView extends KeyboardView {
             return super.onLongPress(key);
         }
     }
-    /*void setSubtypeOnSpaceKey(final InputMethodSubtype subtype) {
-        final Keyboard keyboard = (Keyboard)getKeyboard();
-        keyboard.setSpaceIcon(getResources().getDrawable(subtype.getIconResId()));
-        invalidateAllKeys();
-    }*/
+
 
     @Override
     public void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        //getKeyboard().s
+        /*View focusCurrent = context.getWindow().getCurrentFocus();
+        if( focusCurrent==null || focusCurrent.getClass()!= EditText.class ) return;
+        EditText edittext = (EditText) focusCurrent;
+        Editable editable = edittext.getText();
+        int start = edittext.getSelectionStart();*/
 
         List<Key> keys = getKeyboard().getKeys();
+
         for (Key key : keys) {
+
             Log.e("KEY", "Drawing key with code " + key.codes[0]);
-            if (key.codes[0] == 55002) {
+
+            if (key.codes[0] == 0) {
+                //-- for space around numeric keyboard
+                Drawable dr = (Drawable) context.getResources().getDrawable(R.drawable.circle_black_key);
+                dr.setBounds(key.x, key.y, key.x + key.width, key.y + key.height);
+                dr.draw(canvas);
+
+            } else if (key.codes[0] == 16001 || key.codes[0] == 16002 || key.codes[0] == 16003  || key.codes[0] == 32 ) {
                 //-- for back arrow in numeric keyboard
-                //Log.e("KEY", "Drawing key with code " + key.codes[0]);
                 Drawable dr = (Drawable) context.getResources().getDrawable(R.drawable.rounded_corner_silver_border);
                 dr.setBounds(key.x, key.y, key.x + key.width, key.y + key.height);
                 dr.draw(canvas);
 
-            } else {
+            }else {
+
                 Drawable dr = (Drawable) context.getResources().getDrawable(R.drawable.circle_cyan36);
                 dr.setBounds(key.x, key.y, key.x + key.width, key.y + key.height);
                 dr.draw(canvas);
@@ -74,10 +88,22 @@ public class CustomKeyboardView extends KeyboardView {
 
             //if(key.codes[0]>=97 && key.codes[0]<=122)
 
-            if(key.codes[0]>=97 && key.codes[0]<=122)
-                paint.setTextSize(42);
+            if((key.codes[0]>=97 && key.codes[0]<=122) || key.codes[0] == 64 || key.codes[0] == 63)
+            {
+                paint.setTextSize(42);// querty
+            }
+            else if((key.codes[0]>=48 && key.codes[0]<=57) || key.codes[0] == 15004 )
+            {
+                paint.setTextSize(62);//numeric
+            }
+            else if((key.codes[0]>=65 && key.codes[0]<=90) || key.codes[0] == 32 )
+            {
+                paint.setTextSize(62);//ABC
+            }
             else
+            {
                 paint.setTextSize(62);
+            }
 
             paint.setColor(Color.YELLOW);
 
@@ -85,7 +111,19 @@ public class CustomKeyboardView extends KeyboardView {
                 canvas.drawText(key.label.toString(), key.x + (key.width / 2),
                         key.y + (key.height / 2)+20, paint);
             } else {
-                int padding=30;
+
+                int padding=0;
+
+                //--for ABC
+                if (key.codes[0] == 16001 || key.codes[0] == 16002 || key.codes[0] == 16003  ) {
+                    padding=10;
+                }
+
+                //--for Numeric
+                if (key.codes[0] == 15001 || key.codes[0] == 15002 || key.codes[0] == 15003 || key.codes[0] == 36|| key.codes[0] == -5 ) {
+                    padding=30;
+                }
+
                 key.icon.setBounds(key.x+padding, key.y+padding, key.x + key.width-padding, key.y + key.height-padding);
                 key.icon.draw(canvas);
             }
--- app/src/main/java/com/karmic/simplekeyboard/SimpleIME.java
+++ app/src/main/java/com/karmic/simplekeyboard/SimpleIME.java
@@ -6,6 +6,7 @@ import android.inputmethodservice.KeyboardView;
 import android.media.AudioManager;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputConnection;
 import android.widget.Toast;
 
@@ -17,27 +18,37 @@ public class SimpleIME extends InputMethodService
 
     private boolean caps = false;
 
-    public final static int KBD_NUMBER     = 10001;
-    public final static int KBD_QUERTY     = 10002;
+    public final static int KBD_NUMBER = 10001;
+    public final static int KBD_QUERTY = 10002;
+    public final static int KBD_ABC = 10003;
 
-    private int current_kbd=KBD_NUMBER;
+    private int current_kbd = KBD_NUMBER;
+    private int next_kbd = KBD_ABC;
 
-    public final static int CodeLeft     = 55002;
-    public final static int CodeSearch     = 15001;
-    public final static int CodeMenu     = 15002;
+    //public final static int CodeLeft     = 55002;
+    public final static int CodeSearch = 15001;
+    public final static int CodeMenu = 15002;
+    public final static int CodeDeleteRight = 15003;
+    public final static int CodeNextAbc = 15004;
+
+    public final static int CodeNextNumeric = 16001;
+    public final static int CodeNextHebrew = 16002;
 
 
     @Override
     public View onCreateInputView() {
-        kv = (KeyboardView)getLayoutInflater().inflate(R.layout.keyboard, null);
 
-        if(current_kbd==KBD_NUMBER) {
+        kv = (KeyboardView) getLayoutInflater().inflate(R.layout.keyboard, null);
+
+        if (current_kbd == KBD_NUMBER) {
             keyboard = new Keyboard(this, R.xml.numkbd);
-            current_kbd=KBD_QUERTY;
-        }
-        else if(current_kbd==KBD_QUERTY) {
+            next_kbd = KBD_ABC;
+        } else if (current_kbd == KBD_ABC) {
+            keyboard = new Keyboard(this, R.xml.abckbd);
+            next_kbd = KBD_QUERTY;
+        } else if (current_kbd == KBD_QUERTY) {
             keyboard = new Keyboard(this, R.xml.qwerty);
-            current_kbd=KBD_NUMBER;
+            next_kbd = KBD_NUMBER;
         }
 
         kv.setKeyboard(keyboard);
@@ -47,11 +58,12 @@ public class SimpleIME extends InputMethodService
 
     @Override
     public void onKey(int primaryCode, int[] keyCodes) {
+
         InputConnection ic = getCurrentInputConnection();
         //int start = ic.getSelectionStart();
         playClick(primaryCode);
-        switch(primaryCode){
-            case Keyboard.KEYCODE_DELETE :
+        switch (primaryCode) {
+            case Keyboard.KEYCODE_DELETE:
                 ic.deleteSurroundingText(1, 0);
                 break;
             case Keyboard.KEYCODE_SHIFT:
@@ -62,24 +74,33 @@ public class SimpleIME extends InputMethodService
             case Keyboard.KEYCODE_DONE:
                 ic.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_ENTER));
                 break;
-            case CodeLeft:
-                //ic.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_ENTER));
+            /*case CodeLeft:
                 ic.setSelection(1,0);
+                break;*/
+            case CodeDeleteRight:
+                ic.deleteSurroundingText(0, 1);
                 break;
-
+            case CodeNextAbc:
+                //Toast.makeText(getApplicationContext(),"Next",Toast.LENGTH_SHORT).show();
+                keyboard = new Keyboard(this, R.xml.abckbd);
+                current_kbd = KBD_ABC;
+                next_kbd = KBD_QUERTY;
+                kv.setKeyboard(keyboard);
+                kv.setOnKeyboardActionListener(this);
+                break;
             case CodeMenu:
-                Toast.makeText(getApplicationContext(),"Menu Pressed",Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Menu Pressed", Toast.LENGTH_SHORT).show();
                 break;
             case CodeSearch:
-                Toast.makeText(getApplicationContext(),"Search Pressed",Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Search Pressed", Toast.LENGTH_SHORT).show();
                 break;
 
             default:
-                char code = (char)primaryCode;
-                if(Character.isLetter(code) && caps){
+                char code = (char) primaryCode;
+                if (Character.isLetter(code) && caps) {
                     code = Character.toUpperCase(code);
                 }
-                ic.commitText(String.valueOf(code),1);
+                ic.commitText(String.valueOf(code), 1);
         }
 
     }
@@ -103,45 +124,36 @@ public class SimpleIME extends InputMethodService
     @Override
     public void swipeLeft() {
 
-        if(current_kbd==KBD_NUMBER) {
+        //Toast.makeText(getApplicationContext(),"Left",Toast.LENGTH_SHORT).show();
+        if (next_kbd == KBD_NUMBER) {
             keyboard = new Keyboard(this, R.xml.numkbd);
-            current_kbd=KBD_QUERTY;
-        }
-        else if(current_kbd==KBD_QUERTY) {
+            current_kbd = KBD_NUMBER;
+            next_kbd = KBD_ABC;
+        } else if (next_kbd == KBD_ABC) {
+            keyboard = new Keyboard(this, R.xml.abckbd);
+            current_kbd = KBD_ABC;
+            next_kbd = KBD_QUERTY;
+        } else if (next_kbd == KBD_QUERTY) {
             keyboard = new Keyboard(this, R.xml.qwerty);
-            current_kbd=KBD_NUMBER;
+            current_kbd = KBD_QUERTY;
+            next_kbd = KBD_NUMBER;
         }
-
-        //keyboard = new Keyboard(this, R.xml.qwerty);
         kv.setKeyboard(keyboard);
         kv.setOnKeyboardActionListener(this);
     }
 
     @Override
     public void swipeRight() {
-
-        if(current_kbd==KBD_NUMBER) {
-            keyboard = new Keyboard(this, R.xml.numkbd);
-            current_kbd=KBD_QUERTY;
-        }
-        else if(current_kbd==KBD_QUERTY) {
-            keyboard = new Keyboard(this, R.xml.qwerty);
-            current_kbd=KBD_NUMBER;
-        }
-
-
-        //keyboard = new Keyboard(this, R.xml.numkbd);
-        kv.setKeyboard(keyboard);
-        kv.setOnKeyboardActionListener(this);
+        //Toast.makeText(getApplicationContext(),"Right",Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void swipeUp() {
     }
 
-    private void playClick(int keyCode){
-        AudioManager am = (AudioManager)getSystemService(AUDIO_SERVICE);
-        switch(keyCode){
+    private void playClick(int keyCode) {
+        AudioManager am = (AudioManager) getSystemService(AUDIO_SERVICE);
+        switch (keyCode) {
             case 32:
                 am.playSoundEffect(AudioManager.FX_KEYPRESS_SPACEBAR);
                 break;
@@ -152,9 +164,48 @@ public class SimpleIME extends InputMethodService
             case Keyboard.KEYCODE_DELETE:
                 am.playSoundEffect(AudioManager.FX_KEYPRESS_DELETE);
                 break;
-            default: am.playSoundEffect(AudioManager.FX_KEYPRESS_STANDARD);
+            default:
+                am.playSoundEffect(AudioManager.FX_KEYPRESS_STANDARD);
         }
     }
 
 
+    @Override
+    public void onStartInputView(EditorInfo attribute, boolean restarting) {
+
+        switch (attribute.inputType & EditorInfo.TYPE_MASK_CLASS) {
+            case EditorInfo.TYPE_CLASS_NUMBER:
+                keyboard = new Keyboard(this, R.xml.numkbd);
+                current_kbd = KBD_NUMBER;
+                next_kbd = KBD_ABC;
+                kv.setKeyboard(keyboard);
+                kv.setOnKeyboardActionListener(this);
+
+            case EditorInfo.TYPE_CLASS_PHONE:
+                keyboard = new Keyboard(this, R.xml.numkbd);
+                current_kbd = KBD_NUMBER;
+                next_kbd = KBD_ABC;
+                kv.setKeyboard(keyboard);
+                kv.setOnKeyboardActionListener(this);
+                break;
+
+            case EditorInfo.TYPE_CLASS_TEXT:
+
+                keyboard = new Keyboard(this, R.xml.abckbd);
+                current_kbd = KBD_ABC;
+                next_kbd = KBD_QUERTY;
+                kv.setKeyboard(keyboard);
+                kv.setOnKeyboardActionListener(this);
+                break;
+
+            default:
+
+                keyboard = new Keyboard(this, R.xml.abckbd);
+                current_kbd = KBD_ABC;
+                next_kbd = KBD_QUERTY;
+                kv.setKeyboard(keyboard);
+                kv.setOnKeyboardActionListener(this);
+
+        }
+    }
 }
